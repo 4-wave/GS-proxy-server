b7189d3b59d8ca3ec08a1752337f70830cd90a1b
Gurjot Singh <imasingh@ucdavis.edu>
2019-12-10 09:47:05 -0800

    all components rendering in build mode now, no more bugs. Still need to be positioned

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ var cors = require('cors')
 app.use(cors())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/:id', express.static(path.join(__dirname, '../public')))
 
-app.get('/app1',function(req,res) {
-    res.send("Hello world From Server 1");
-});
-
-
-app.listen(3001);
+app.listen(3000);
